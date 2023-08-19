64e52be591d41349bc661fdf1652f543aed3785b
Subash_Chandra <s.chandra106@gmail.com>
2023-08-19 07:03:48 +0530

    modified responsive for purchase and order pages

--- src/components/Purchases/index.js
+++ src/components/Purchases/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import { TextField, Button } from '@mui/material';
 import moment from 'moment';
+import './style.css';
 
 const Purchases = () => {
   const [purchase, setPurchase] = useState({ date: moment().format('YYYY-MM-DD'), amount: '' });
@@ -28,21 +29,21 @@ const Purchases = () => {
   };
 
   return (
-    <div className="orders">
+    <div className="purchases">
       <div className="bg-white" style={{ padding: '2rem', borderRadius: '1.2rem' }}>
         <h1 className="heading-primary">Purchases</h1>
         <form onSubmit={submitHandler}>
           <Grid container spacing={2}>
-            <Grid item xs={3}>
+            <Grid item xs={12} md={3}>
               <TextField onChange={chnageHandler} name="date" type="date" id="date" value={date} />
             </Grid>
-            <Grid item xs={3}>
+            <Grid item xs={12} md={3}>
               <TextField onChange={chnageHandler} name="amount" type="number" id="amount" value={amount} />
             </Grid>
-            <Grid item xs={3}>
+            <Grid item xs={12} md={3}>
               file to upload
             </Grid>
-            <Grid item xs={3}>
+            <Grid item xs={12} md={3}>
               <Button onClick={submitHandler} variant="contained" color="success">
                 Save
               </Button>
