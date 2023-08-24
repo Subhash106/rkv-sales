5db5f883ba3ee70bea651a11f62649e6fd4268ed
Subash_Chandra <s.chandra106@gmail.com>
2023-08-24 06:24:59 +0530

    Linked purchase summary on dashboard to purchases

--- src/components/Dashboard/PurchasesSummary/index.js
+++ src/components/Dashboard/PurchasesSummary/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import { number } from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import moment from 'moment';
 
-export default function PurchasesSummary({ purchasesCount, purchasesAmount }) {
+export default function PurchasesSummary({ purchasesCount, purchasesAmount, month, year }) {
   return (
     <Card className="p-sm" variant="outlined">
       <h2 className="mb-sm">Purchases Summary</h2>
       <dl>
         <dt>Total Purchases</dt>
         <dd className="mb-xs">
-          <strong>{purchasesCount}</strong>
+          <strong>
+            <NavLink to={`/purchases?month=${month}&year=${year}`}> {purchasesCount}</NavLink>
+          </strong>
         </dd>
         <dt>Total Amount</dt>
         <dd>
@@ -22,5 +26,12 @@ export default function PurchasesSummary({ purchasesCount, purchasesAmount }) {
 
 PurchasesSummary.propTypes = {
   purchasesCount: number.isRequired,
-  purchasesAmount: number.isRequired
+  purchasesAmount: number.isRequired,
+  month: number,
+  year: number
+};
+
+PurchasesSummary.defaultProps = {
+  month: moment().month() + 1,
+  year: moment().year()
 };
--- src/components/Purchases/PurchasesTable.js
+++ src/components/Purchases/PurchasesTable.js
@@ -6,14 +6,26 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useSearchParams } from 'react-router-dom';
+import { Grid } from '@mui/material';
+
 import { useGetPurchasesQuery } from '../../store/base';
 
 export default function PurchasesTable() {
+  const [params] = useSearchParams();
   const { error, isFetching, data = {} } = useGetPurchasesQuery();
+  let rows = [];
+  const month = params.get('month');
+  const year = params.get('year');
 
-  const rows = Object.values(data);
-
-  console.log('rows', rows);
+  if (month && year) {
+    rows = Object.values(data).filter(purchase => {
+      const dateArray = purchase?.date?.split('-');
+      return +month === +dateArray?.[1] && +year === +dateArray?.[0];
+    });
+  } else {
+    rows = Object.values(data);
+  }
 
   if (error) {
     return <p>Oops, Something went wrong!</p>;
@@ -24,30 +36,37 @@ export default function PurchasesTable() {
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell align="right">Amount</TableCell>
-            <TableCell align="right">Invoice</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.length > 0 &&
-            rows.map(row => (
-              <TableRow key={`${row.date}_${row.amount}`} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell>{row.date}</TableCell>
-                <TableCell align="right">{row.amount}</TableCell>
-                <TableCell align="right">
-                  <a download={row.invoiceName} href={row.invoice}>
-                    {row.invoiceName}
-                  </a>
-                </TableCell>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell align="right">Amount</TableCell>
+                <TableCell align="right">Invoice</TableCell>
               </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            </TableHead>
+            <TableBody>
+              {rows.length > 0 &&
+                rows.map(row => (
+                  <TableRow
+                    key={`${row.date}_${row.amount}`}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell>{row.date}</TableCell>
+                    <TableCell align="right">{row.amount}</TableCell>
+                    <TableCell align="right">
+                      <a download={row.invoiceName} href={row.invoice}>
+                        {row.invoiceName}
+                      </a>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   );
 }
