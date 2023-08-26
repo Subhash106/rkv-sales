bdd926fc455ac182747f6a6ff7cd189b92bda5b7
Subash_Chandra <s.chandra106@gmail.com>
2023-08-26 09:10:31 +0530

    added side menu for mobile view

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate, NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAuth, signOut } from 'firebase/auth';
@@ -9,6 +9,9 @@ import { logout } from '../../store/auth';
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const hamburgerRef = useRef();
+  const topnavRef = useRef();
+  const closeRef = useRef();
   const token = useSelector(state => state.auth.idToken) || localStorage.getItem('token');
   const logoutHandler = e => {
     e.preventDefault();
@@ -27,17 +30,36 @@ const Header = () => {
         console.error('logout error', error);
       });
   };
+
+  const hamburgerClickHandler = () => {
+    topnavRef.current.style.display = 'block';
+  };
+
+  const closeClickHandler = () => {
+    topnavRef.current.style.display = 'none';
+  };
+
   return (
     <div className="header">
       <div className="container">
         <header className="main-header">
+          <div onClick={hamburgerClickHandler} ref={hamburgerRef} role="button" tabIndex={0} className="hamburger">
+            <div className="hamburger--top"></div>
+            <div className="hamburger--middle"></div>
+            <div className="hamburger--bottom"></div>
+          </div>
           <div className="logo-box">
             <NavLink to="/">
               <img src="../../../img/logo.jpg" alt="Logo" className="logo" />
             </NavLink>
           </div>
 
-          <div className="top-nav">
+          <div ref={topnavRef} className="top-nav">
+            <div onClick={closeClickHandler} ref={closeRef} role="button" tabIndex={0} className="close">
+              <div className="close--top"></div>
+              <div className="close--middle"></div>
+              <div className="close--bottom"></div>
+            </div>
             <ul>
               <li>
                 <NavLink
