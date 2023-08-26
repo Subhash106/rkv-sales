c37453ba901cd5fcece22126de9a72369a15a185
Subash_Chandra <s.chandra106@gmail.com>
2023-08-26 07:04:23 +0530

    route adjustment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
@@ -10,51 +11,58 @@ import Purchases from './components/Purchases';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
+import store from './store';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
     errorElement: <Error />,
     children: [
+      { index: true, element: <Login /> },
       {
-        index: true,
-        element: <Login />
-      },
-      {
-        path: '/dashboard',
-        element: (
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        )
-      },
-      {
-        path: '/sales',
-        element: (
-          <ProtectedRoute>
-            <Sales />
-          </ProtectedRoute>
-        )
-      },
-      {
-        path: '/orders',
-        element: (
-          <ProtectedRoute>
-            <Orders />
-          </ProtectedRoute>
-        )
-      },
-      {
-        path: '/purchases',
-        element: (
-          <ProtectedRoute>
-            <Purchases />
-          </ProtectedRoute>
-        )
+        element: <App />,
+        children: [
+          {
+            path: '/dashboard',
+            element: (
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            )
+          },
+          {
+            path: '/sales',
+            element: (
+              <ProtectedRoute>
+                <Sales />
+              </ProtectedRoute>
+            )
+          },
+          {
+            path: '/orders',
+            element: (
+              <ProtectedRoute>
+                <Orders />
+              </ProtectedRoute>
+            )
+          },
+          {
+            path: '/purchases',
+            element: (
+              <ProtectedRoute>
+                <Purchases />
+              </ProtectedRoute>
+            )
+          }
+        ]
       }
     ]
   }
 ]);
 
-ReactDOM.render(<RouterProvider router={router} />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+  document.getElementById('root')
+);
